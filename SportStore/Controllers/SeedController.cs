76c2606698cde1d0d87dfbbd05f50bb881a0449d
artik3314@gmail.com <artik3314@gmail.com>
2023-01-24 21:42:48 +0300

    Генерация тестовых данных через отдельный контроллер. Зпросы делаюься через EF на чистом SQL

---
+++ SportStore/Controllers/SeedController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportStore.Data;
+
+namespace SportStore.Controllers
+{
+    public class SeedController : Controller
+    {
+        private DataContext context;
+
+        public SeedController(DataContext ctx) => context = ctx;
+
+        public IActionResult Index()
+        {
+            ViewBag.Count = context.Products.Count();
+
+            return View(context.Products
+                .Include(p => p.Category).OrderBy(p => p.Id).Take(20));
+        }
+
+        [HttpPost]
+        public IActionResult CreateSeedData(int count)
+        {
+            ClearData();
+
+            if(count > 0)
+            {
+                context.Database.SetCommandTimeout(System.TimeSpan.FromMinutes(10));
+                context.Database.ExecuteSqlRaw("Drop procedure if exists CreateSeedData");
+
+
+                context.Database.ExecuteSqlRaw($@"
+
+                create procedure CreateSeedData
+                @RowCount decimal
+                as
+                    begin
+                        set nocount on
+                        declare @i int = 1;
+                        declare @catId Bigint;
+                        declare @catCount int = @rowCount / 10;
+                        declare @pprice decimal(5,2);
+                        declare @rprice decimal(5,2);
+
+                        begin transaction
+                            while @i <= @CatCount
+                                begin
+                                    insert into Categories(Name, Description)
+                                    values (Concat('Category-', @i),'Test Data Category');
+                                set @catId = Scope_Identity();
+                                declare @j int = 1;
+                                while @j <= 10
+                                    begin
+                                       set @pprice = rand()*(500-5+1);
+                                       set @rprice = (rand()*@pprice) + @pprice
+                                       insert into Products(Name,CategoryId,PurchasePrice,RetailPrice)
+                                       values(concat('Product',@i,'-',@j),@catId,@pprice,@rprice)
+                                       set @j=@j+1
+                                    end
+                                    set @i=@i+1
+                                end
+                                commit
+                end"
+
+);
+
+                context.Database.BeginTransaction();
+                context.Database.ExecuteSqlRaw($"EXEC CreateSeedData @RowCount = {count}");
+                context.Database.CommitTransaction();
+
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult ClearData()
+        {
+            context.Database.SetCommandTimeout(System.TimeSpan.FromMinutes(10));
+            context.Database.BeginTransaction();
+            context.Database.ExecuteSqlRaw("delete from orders");
+            context.Database.ExecuteSqlRaw("delete from categories");
+            context.Database.CommitTransaction();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
